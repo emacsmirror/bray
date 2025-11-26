3b76874975f2cf973189700249271f4a84a94f84
Campbell Barton <ideasman42@gmail.com>
2025-11-26 16:21:27 +1100

    Cleanup: use null instead of not

--- examples/simple/init.el
+++ examples/simple/init.el
@@ -140,7 +140,7 @@
 (add-hook
  'after-change-major-mode-hook
  (lambda ()
-   (when (and (not (minibufferp)) (not (derived-mode-p 'special-mode)))
+   (when (and (null (minibufferp)) (null (derived-mode-p 'special-mode)))
      (bray-mode))))
 
 ;; Local Variables:
